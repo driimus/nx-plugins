b287ccbffc3f26f17b69ef50c287b0bedf1b5194
Cristian Petre <driimus@vivaldi.net>
2024-05-12 01:14:39 +0100

    chore: cleanup

--- tools/scripts/start-local-registry.ts
+++ tools/scripts/start-local-registry.ts
@@ -2,20 +2,29 @@
  * This script starts a local registry for e2e testing purposes.
  * It is meant to be called in jest's globalSetup.
  */
+import type { Config } from '@jest/types';
 import { startLocalRegistry } from '@nx/js/plugins/jest/local-registry';
-import { execFileSync } from 'child_process';
 import { releasePublish, releaseVersion } from 'nx/release';
+import { join } from 'path';
+import { tmpdir } from 'tmp';
 
-export default async () => {
+export default async (globalConfig: Config.GlobalConfig) => {
   // local registry target to run
   const localRegistryTarget = 'root:local-registry';
   // storage folder for the local registry
   const storage = './tmp/local-registry/storage';
 
+  // global.stopLocalRegistry = await startLocalRegistry({
+  //   localRegistryTarget,
+  //   storage,
+  //   verbose: false,
+  // });
+  const isVerbose: boolean = process.env.NX_VERBOSE_LOGGING === 'true' || !!globalConfig.verbose;
+  const storageLocation = join(tmpdir, 'local-registry/storage', process.env.NX_TASK_TARGET_PROJECT ?? '');
   global.stopLocalRegistry = await startLocalRegistry({
     localRegistryTarget,
-    storage,
-    verbose: false,
+    verbose: isVerbose,
+    storage: storageLocation,
   });
 
   await releaseVersion({
