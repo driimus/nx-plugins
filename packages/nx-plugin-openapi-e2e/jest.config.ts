16515c246861b51759597efa7ae7fbf985d1acbd
Cristian Petre <driimus@vivaldi.net>
2025-02-11 00:02:29 +0000

    chore: migrate to latest nx

--- packages/nx-plugin-openapi-e2e/jest.config.ts
+++ packages/nx-plugin-openapi-e2e/jest.config.ts
@@ -3,6 +3,7 @@ import { Config } from 'jest';
 
 export default {
   displayName: 'nx-plugin-openapi-e2e',
+  testEnvironment: 'node',
   preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
